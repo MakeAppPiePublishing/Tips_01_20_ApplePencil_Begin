0a9e277d468bff0f6ad24657ff5430756561e874
Steve Lipton <stevelipton@BRK-16892.local>
2018-04-20 05:13:06 -0700

    first Commit

---
+++ ApplePencil.playground/Contents.swift
@@ -0,0 +1,77 @@
+import UIKit
+import PlaygroundSupport
+class ViewController:UIViewController{
+    
+    //Views
+    let square = UILabel()
+    let statusLabel = UILabel()
+    //touch object
+    var touch = UITouch()
+    
+    var squareText = "Untouched"
+    var statusText = "Touch And Pencil Demo"
+    var force:CGFloat = 0.0
+    var azimuth:CGFloat = 0.0
+    var location = CGPoint(x: 200, y: 200)
+    var squareSize = CGSize(width: 100, height: 100)
+    var altitude:CGFloat = 0.0
+    
+    //radian conversions
+    func degrees(_ radians:CGFloat)->CGFloat{
+        return radians * (180.0 / CGFloat.pi)
+    }
+    
+    func updateDisplay(touches: Set<UITouch>){
+        // add your code here
+        
+        //update the labels
+        statusText = String(format:"Stylus: X:%3.0f Y:%3.0f Azimuth:%2.3f(%2.1fº) Altitude:%2.3f(%2.1fº) Force:%2.3f",location.x,location.y,azimuth,degrees(azimuth),altitude,degrees(altitude),force)
+        statusLabel.text = statusText
+        square.frame.origin = location
+        square.text = squareText
+    }
+    
+    //MARK: - Touch events
+    
+    
+    //MARK: -- Label configuration
+    func addSquare(){
+        view.addSubview(square)
+        square.frame = CGRect(origin: location, size: squareSize)
+        square.backgroundColor = .blue
+        square.text = "untouched"
+        square.textColor = .white
+        square.textAlignment = .center
+    }
+    
+    func addStatusLabel(){
+        view.addSubview(statusLabel)
+        statusLabel.text = "Touch and Pencil Demo"
+        statusLabel.font = UIFont(name: "Menlo", size: 20)
+        statusLabel.backgroundColor = UIColor.darkGray.withAlphaComponent(50)
+        statusLabel.textColor = .white
+        statusLabel.translatesAutoresizingMaskIntoConstraints = false
+        var constraints = [NSLayoutConstraint]()
+        constraints += [NSLayoutConstraint(item: statusLabel, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 0.0)]
+        constraints += [NSLayoutConstraint(item: statusLabel, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: 0.0)]
+        constraints += [NSLayoutConstraint(item: statusLabel, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1.0, constant: 0.0)]
+        view.addConstraints(constraints)
+    }
+    
+    
+    //MARK: -- Life Cycle 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        addStatusLabel()
+        addSquare()
+
+        
+
+    }
+    
+}
+
+let vc = ViewController()
+PlaygroundPage.current.liveView = vc
